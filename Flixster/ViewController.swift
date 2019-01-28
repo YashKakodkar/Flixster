c2b56adb4a54b86e722c15d36d07ba13e8260283
Yash Kakodkar <ymac@yashs-macbook-pro.local>
2019-01-28 17:43:04 -0600

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Yash Kakodkar on 1/28/19.
+//  Copyright © 2019 Yash Kakodkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
